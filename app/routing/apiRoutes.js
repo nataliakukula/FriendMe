fe80fcb17b04da765426719f3a781d6815192062
Kinky Cookie <kuki_kukula@yahoo.com>
2018-12-06 18:12:31 -0600

    herokuuuu

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,21 +14,28 @@ module.exports = app => {
         // Determine the user's most compatible friend using the following as a guide:
         let yourScore = req.body.score;
 
-        // Convert each user's results into a simple array of numbers:       
-        // const scoreNums = yourScore.map(x => +x); // Math.abs() recognizes the num-sting as a number
+        // Need to convert user string inputs in scores to numbers
+        let scoreNums = yourScore.map(x => +x);
+
+        let newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: scoreNums
+        }
 
         let perfectMatchIndex = 0; // Need a starting index of the element
         let maximumDifference = 40; // Need a 'maximum' possible difference that represence the worst match
 
         // Check the total differences of all posible matches against the user -> Math.abs() get the absolute value of a number
         for (let i = 0; i < friends.length; i++) {
+            console.log("friends scores", friends[i].scores);
+
             let totalDifference = 0;
-            // TODO error - cannot read length of undefined
+
             for (let j = 0; j < friends[i].scores.length; j++) {
-                let difference = Math.abs(yourScore[j] - friends[i].scores[j]);
+                let difference = Math.abs(scoreNums[j] - friends[i].scores[j]);
                 totalDifference += difference;
             }
-            // console.log("total difference:", totalDifference);
 
             // If there is a lower minimum - change the index, if not keep it as is
             if (totalDifference < maximumDifference) {
@@ -37,12 +44,10 @@ module.exports = app => {
             }
         }
 
-        // console.log("index: ", perfectMatchIndex);
-
         // Send back to browser the best friend match
         res.json(friends[perfectMatchIndex]);
 
         // Add user to friend array:
-        friends.push(req.body);
+        friends.push(newFriend);
     });
 };
